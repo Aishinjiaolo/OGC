400fce78c2fb64196d6c1fdeefec69ca0a7be8aa
ktlo <roggerbass@gmail.com>
2011-11-26 21:01:38 +0800

    separate main flow and point function library
    TODO:
    create makefile
        1. build shared function library
        2. compile main and library as a run file

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "point.h"
+
+int main() {
+    // profiler
+    Profiler profiler;
+    profiler.start();
+
+    // random generate 1d vector
+    // assign maximum size =>
+    const int max = 100;
+    vector<double> main_1d_points(max);
+    
+    srand(time(NULL));
+    for (int i = 0; i < max; i++) {
+        main_1d_points[i] = rand() % 100;
+    };
+
+    // transform to Points, then sorting
+    Points main_points;
+    main_points.parse_1d_points_from(main_1d_points);
+    main_points.sort_by_y();
+    
+    for (int i = 0; i < main_points.size(); i++) {
+        cout << main_points.get_point(i).get_gx() << "\t"
+            << main_points.get_point(i).get_gy() << endl;
+    }
+
+    Points center;
+    center.create_center_from(main_points);
+
+    for (int i = 0; i < center.size(); i++) {
+        cout << center.get_point(i).get_gx() << "\t"
+            << center.get_point(i).get_gy() << endl;
+    }
+
+    profiler.end();
+    
+    return 0;
+}
