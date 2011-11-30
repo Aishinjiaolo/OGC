cb62cb574732147de8377a9527e7b948c13c1f36
ktlo <roggerbass@gmail.com>
2011-12-01 07:17:21 +0800

    write more unit test
    yet not so unit

--- test/point_unittest.cc
+++ test/point_unittest.cc
@@ -3,7 +3,8 @@
 
 TEST(PointTest, SetPoint) {
     srand(time(NULL));
-    for (int i = 0 ; i < 100; i++) {
+    int max = 1000000;
+    for (int i = 0 ; i < max; i++) {
         Point test;
         double x = rand() % 100;
         double y = rand() % 100;
@@ -12,6 +13,23 @@ TEST(PointTest, SetPoint) {
         EXPECT_EQ(x, test.get_gx());
         EXPECT_EQ(y, test.get_gy());
     }
+}
+
+TEST(PointsTest, EmptySize) {
+    Points points;
+    EXPECT_EQ(0, points.size());
+}
+
+TEST(PointsTest, PointSize) {
+    srand(time(NULL));
+    int size = rand() % 1000000;
+    Points points;
+    for (int i = 0 ; i < size; i++) {
+        Point test;
+        points.append(test);
+    }
+
+    EXPECT_EQ(size, points.size());
 }
 
 
--- test/segment_unittest.cc
+++ test/segment_unittest.cc
@@ -3,19 +3,20 @@
 
 TEST(SegmentTest, SetSegment) {
     srand(time(NULL));
-    for (int i = 0; i < 100; i++) {
+    int size = 1000000;
+    for (int i = 0; i < size; i++) {
         double x1 = rand() % 100;
         double y1 = rand() % 100;
         double x2 = rand() % 100;
         double y2 = rand() % 100;
-        
+
         Point head, tail;
         head.set_point(x1, y1);
         tail.set_point(x2, y2);
-        
+
         Segment segment;
         segment.set_segment(&head, &tail);
-        
+
         EXPECT_EQ(x1, segment.get_head()->get_gx());
         EXPECT_EQ(y1, segment.get_head()->get_gy());
         EXPECT_EQ(x2, segment.get_tail()->get_gx());
@@ -23,4 +24,42 @@ TEST(SegmentTest, SetSegment) {
     }
 }
 
+TEST(SegmentTest, GetLength) {
+    srand(time(NULL));
+    int size = 1000000;
+    for (int i = 0; i < size; i++) {
+        double length = rand() % 100000;
+        double x1 = rand() % 100;
+        double y1 = rand() % 100;
+        double x2 = x1;
+        double y2 = y1 + length;
 
+        Point head, tail;
+        head.set_point(x1, y1);
+        tail.set_point(x2, y2);
+
+        Segment segment;
+        segment.set_segment(&head, &tail);
+
+        EXPECT_EQ(length, segment.get_length());
+        EXPECT_TRUE(segment.get_length() >= 0);
+    }
+
+    for (int i = 0; i < size; i++) {
+        double length = rand() % 100000;
+        double x1 = rand() % 100;
+        double y1 = rand() % 100;
+        double x2 = x1 + length;
+        double y2 = y1;
+
+        Point head, tail;
+        head.set_point(x1, y1);
+        tail.set_point(x2, y2);
+
+        Segment segment;
+        segment.set_segment(&head, &tail);
+
+        EXPECT_EQ(length, segment.get_length());
+        EXPECT_TRUE(segment.get_length() >= 0);
+    }
+}
